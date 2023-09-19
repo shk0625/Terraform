87eb3f9c8529732bb2af3d445184c0e987ed5059
shk0625 <tjfgml1238@gmail.com>
2023-09-19 19:50:44 +0900

    ec2.tf에 가용영역 추가

--- provisioning/ec2.tf
+++ provisioning/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Test1ec2" {
     ami = "ami-0454bb2fefc7de534"
     instance_type = "t2.micro"
+    availability_zone = "ap-northeast-2a"
 
     tags = {
         "Name" = "Created By Terraform"
